b2693e8b9f665a56cd1fd8b47ffec2845b5089ac
skyonide <skye.grossman@gmail.com>
2025-10-24 09:01:35 -0500

    pull test 9

--- routes/logfileServer.ts
+++ routes/logfileServer.ts
@@ -8,19 +8,12 @@ import { Request, Response, NextFunction } from 'express'
 
 module.exports = function serveLogFiles () {
   return ({ params }: Request, res: Response, next: NextFunction) => {
-
     const file = params.file
-
     if (!file.includes('/')) {
-
       res.sendFile(path.resolve('logs/', file))
-
     } else {
-
       res.status(403)
       next(new Error('File names cannot contain forward slashes!'))
-
     }
   }
-
 }
